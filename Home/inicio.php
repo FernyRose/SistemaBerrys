96363f1d7740c12f31c8f049eb2ea1eaa6687b61
FernyRose <129886205+FernyRose@users.noreply.github.com>
2023-04-09 03:06:27 -0500

    nav edit

--- Home/inicio.php
+++ Home/inicio.php
@@ -9,7 +9,72 @@
     <title>Inicio</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav mx-auto">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Pricing</a>
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </div>
+    </div>
+  </div>
+</nav>
 <nav class="navbar navbar-expand-lg navbar-light bg-danger">
+            <div class="container-fluid">
+              <a class="navbar-brand text-light" href="inicio.php">Sistema berrys</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active text-light" aria-current="page" href="inicio.php">Inicio</a>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Tienda
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          
+                    </ul>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Nuestras tiendas
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    
+                      <li><hr class="dropdown-divider"></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Contacto$$$
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    
+                      <li><hr class="dropdown-divider"></li>
+                    </ul>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active text-light " aria-current="page" href="#">Ayuda</a>
+                  </li>
+                </ul>
+                <form class="d-flex ms-auto">
+                <p class="text-light"></p>
+                  <a href="salir.php"><input type="button" value="Salir" onclick="return res()" class="btn btn-primary"></a>
+                </form>
+              </div>
+            </div>
+          </nav>
+
+          <nav class="navbar navbar-expand-lg navbar-light bg-danger">
             <div class="container-fluid">
               <a class="navbar-brand text-light" href="inicio.php">Sistema berrys</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -70,8 +135,8 @@
                     <a class="nav-link active text-light " aria-current="page" href="#">Ayuda</a>
                   </li>
                 </ul>
-                <form class="d-flex">
-                <p class="text-light">&nbsp;&nbsp;&nbsp;&nbsp;</p>
+                <form class="d-flex ms-auto">
+                <p class="text-light"></p>
                   <a href="salir.php"><input type="button" value="Salir" onclick="return res()" class="btn btn-primary"></a>
                 </form>
               </div>
