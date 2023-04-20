fb873b42b1ade37f7d21e184e6592dde42ef5c87
Joel Zepeda <joe621.jayz@gmail.com>
2023-04-19 23:02:48 -0600

    Fase 2 landing

--- Home/inicio.php
+++ Home/inicio.php
@@ -6,14 +6,17 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" href="../bootstrap-5.1.3-dist/css/bootstrap.min.css">
       <link rel="stylesheet" href="../Datatables-1.11.3/css/dataTables.bootstrap5.min.css">
+      <link rel="stylesheet" href="./main.scss">
       <script src="../bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
-      
       <link rel="stylesheet" href="../Imagenes/tecnm.png">
       <style>
         .navbar-brand{
           width: 98vw;
           position: absolute;
           top: 0;
+        }
+        .navbar{
+          text-shadow: #a4232c;
         }
         .navbar-brand img{
           display: block;
@@ -55,25 +58,36 @@
       <title>Inicio</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-$blue-100  bg-dark">
+    <h1>
+    <div
+          class="p-5 text-center bg-image"
+          style="
+            background-image: url('../Imagenes/fondo2.jpg'); 
+            height: 2000px;
+            max-width: auto;
+          "
+        >
+      
+    
+    <nav class="navbar navbar-expand-lg navbar-dark">
       <div class="container-fluid">
         <a class="navbar-brand" href="#"><img src="../Imagenes/tecnm.png"></a>
         <div class="collapse navbar-collapse m-x" id="top navbar">
           <div class="navbar-nav">
-            <button type="button" class="btn btn-primary">
+            <button type="button" class="btn btn-primary text-center">
               Sign up
             </button>
           </div>
         </div>
       </div>
     </nav>
-    <nav class="navbar navbar-expand-lg navbar-$blue-100 ">
+    <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-               <div class="navbar-nav m-auto  fs-3">
+               <div class="navbar-nav m-auto  fs-3 text-info">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                     <a class="nav-link" href="#">Features</a>
                     <a class="nav-link" href="#">Pricing</a>
@@ -89,31 +103,30 @@
           style="
             background-image: url('../Imagenes/berrys3.jpg');
             height: 700px;
+            width: auto;
             margin-top: 58px;
             margin-bottom: 58px;
+            
           "
         >
-          <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+          <div class="mask" style="background-color: rgba(0, 0, 0, 0.1);">
             <div class="d-flex justify-content-center align-items-center h-100">
               <div class="text-white">
-                <h1 class="mb-3">Heading</h1>
-                <h4 class="mb-3">Subheading</h4>
-                <a class="btn btn-outline-light btn-lg" href="#!" role="button"
-                >Call to action</a
-                >
+                <h1 class="mb-3">Pedidos a toda la ciudad</h1>
+                <h4 class="mb-3">As tu pedido ya</h4>
               </div>
             </div>
           </div>
         </div>
         <div>
-          <div class="card mb-3" style="max-width: 540px;">
+          <div class="card mb-3 m-auto" style="max-width: auto; background-color: rgba(0, 0, 0, 0.1);">
             <div class="row g-0">
               <div class="col-md-4">
-                <img src="../Imagenes/Logo.jpg" class="img-fluid rounded-start" alt="...">
+                <img src="../Imagenes/clancheAradano.jpeg" class="img-fluid rounded-start" alt="...">
               </div>
               <div class="col-md-8">
                 <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
+                  <h5 class="card-title">Card title 1</h5>
                   <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                   <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
                 </div>
@@ -128,7 +141,7 @@
         <div class="carousel-item active">
           <div class="card">
             <div class="image-wrapper">
-              <img src="../Imagenes/Logo.jpg" alt="...">
+              <img src="../Imagenes/CajaAradanos.png" alt="...">
             </div>
             <div class="card-body">
               <h5 class="card-title">Card title</h5>
@@ -140,7 +153,7 @@
         <div class="carousel-item">
           <div class="card">
               <div class="image-wrapper">
-                <img src="../Imagenes/Logo.jpg""  alt="...">
+                <img src="../Imagenes/clancheZarzamora.jpg""  alt="...">
               </div>
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
@@ -152,7 +165,7 @@
         <div class="carousel-item">
           <div class="card">
               <div class="image-wrapper">
-                <img src="../Imagenes/Logo.jpg" alt="...">
+                <img src="../Imagenes/CajaAradanos.png" alt="...">
               </div>
               <div class="card-body">
                 <h5 class="card-title">Card title</h5>
@@ -171,6 +184,7 @@
         <span class="visually-hidden">Next</span>
       </button>
   </div>
-    
+  </div>
+  </h1>
   </body>
 </html>
