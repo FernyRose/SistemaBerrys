806f4750ab01ff0d5ee63d78f61f6d152e68886f
FernyRose <129886205+FernyRose@users.noreply.github.com>
2023-04-23 11:49:13 -0600

    Update inicio.php

--- Home/inicio.php
+++ Home/inicio.php
@@ -22,9 +22,6 @@
           display: block;
           margin: auto;
         }
-        .btn{
-          margin: ;
-        }
         .carousel-inner{
           display: flex;
         }
@@ -88,14 +85,14 @@
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
-             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-               <div class="navbar-nav m-auto  fs-3 text-dark">
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav m-auto  fs-3 text-dark">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                     <a class="nav-link" href="#">Features</a>
                     <a class="nav-link" href="#">Pricing</a>
                     <a class="nav-link" href="#">Disabled</a>
-                </div>
-                
+              </div>
+            </div>   
         </div>
     </nav>
     <div class="contaner my-5">
