958604a8940bcccbf22958f699d02a42f25cccf9
JoelZepeda <joe621.jayz@gmail.com>
2023-04-23 11:06:25 -0600

    Parte 4 Landing

--- Home/inicio.php
+++ Home/inicio.php
@@ -28,57 +28,59 @@
         .carousel-inner{
           display: flex;
         }
-        .carousel-item{
-          display: block;
-          margin-right: 0;
-          flex: 0 0 calc(100%/3);
+        @media screen and (min-width: 568px) {
+          
+          .card{
+            margin: 0 .5em;
+            width: calc(100%/3);
+          }
+          .cards-wrapper{
+            display: flex;
+          }
+          .image-wrapper{
+            height: 22vw;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+
+        }  
+        @media screen and (max-width: 567px) {
+          .card:not(:first-child){
+            display: none;
+          }
         }
-        .carousel-inner{
-          padding: 1em;
-        }
-        .card{
-          margin: 0 .5em;
-        }
-        .img{
+        
+        .image--wrapper img{
           max-width: 100%;
           max-height: 100%;
-        }
-        .image-wrapper{
-          max-width: 100%;
-          height: 20em;
-          display: flex;
-          justify-content: center;
-          align-items: flex-start;
-        }
-        .img-wrapper{
-          height: 23vw;
         }
       </style>
         
       <title>Inicio</title>
   </head>
   <body>
-    <h1>
-    <div
-          class="p-5 text-center bg-image"
-          style="
-            background-image: url('../Imagenes/fondo2.jpg'); 
-            height: 2000px;
-            max-width: auto;
-          "
-        >
-      
     
-    <nav class="navbar navbar-expand-lg navbar-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="../Imagenes/tecnm.png"></a>
-        <div class="collapse navbar-collapse m-x" id="top navbar">
-          <div class="navbar-nav">
-            <button type="button" class="btn btn-primary text-center">
-              Sign up
-            </button>
+      <div
+            class="p-0 text-center bg-image"
+            style="
+              background-image: url('../Imagenes/fondo2.jpg'); 
+              backgorund-size: cover;
+              min-height: 60vh;
+            "
+          >
+        
+      
+      <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img src="../Imagenes/tecnm.png"></a>
+          <div class="collapse navbar-collapse m-x" id="top navbar">
+            <div class="navbar-nav">
+              <button type="button" class="btn btn-primary text-center">
+                Sign up
+              </button>
+            </div>
           </div>
-        </div>
       </div>
     </nav>
     <nav class="navbar navbar-expand-lg navbar-light">
@@ -87,7 +89,7 @@
             <span class="navbar-toggler-icon"></span>
             </button>
              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-               <div class="navbar-nav m-auto  fs-3 text-info">
+               <div class="navbar-nav m-auto  fs-3 text-dark">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                     <a class="nav-link" href="#">Features</a>
                     <a class="nav-link" href="#">Pricing</a>
@@ -103,8 +105,8 @@
           style="
             background-image: url('../Imagenes/berrys3.jpg');
             height: 700px;
-            width: auto;
-            margin-top: 58px;
+            width: 100%;
+            margin-top: 45px;
             margin-bottom: 58px;
             
           "
@@ -119,10 +121,10 @@
           </div>
         </div>
         <div>
-          <div class="card mb-3 m-auto" style="max-width: auto; background-color: rgba(0, 0, 0, 0.1);">
+          <div class="card2 mb-3 m-auto" style="max-width: auto; background-color: rgba(0, 0, 0, 0.1);">
             <div class="row g-0">
               <div class="col-md-4">
-                <img src="../Imagenes/clancheAradano.jpeg" class="img-fluid rounded-start" alt="...">
+                <img src="../Imagenes/blueberries.png" class="img-fluid rounded-start" alt="...">
               </div>
               <div class="col-md-8">
                 <div class="card-body">
@@ -136,55 +138,124 @@
         </div>
       </h2>
     </div>
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+
+    <div id="carouselExampleInterval" class="carousel carousel-dark slide" data-bs-ride="carousel">
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <div class="card">
-            <div class="image-wrapper">
-              <img src="../Imagenes/CajaAradanos.png" alt="...">
+          <div class="cards-wrapper">
+            <div class="card">
+              <div class="image-wrapper">
+                <img src="../Imagenes/blue4.jpg" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">1</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
             </div>
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="card">
+              <div class="image-wrapper">
+                <img src="../Imagenes/ble3.png" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">2</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+            <div class="card">
+              <div class="image-wrapper">
+                <img src="../Imagenes/ble3.png" alt="...">
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">3</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
             </div>
           </div>
         </div>
         <div class="carousel-item">
-          <div class="card">
-              <div class="image-wrapper">
-                <img src="../Imagenes/clancheZarzamora.jpg""  alt="...">
+          <div class="cards-wrapper">
+            <div class="card">
+                <div class="image-wrapper">
+                  <img src="../Imagenes/ble3.png" alt="...">
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">4</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
               </div>
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+              <div class="card">
+                <div class="image-wrapper">
+                  <img src="../Imagenes/ble3.png" alt="...">
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">5</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
               </div>
+              <div class="card">
+                <div class="image-wrapper">
+                  <img src="../Imagenes/ble3.png" alt="...">
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">6</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
           </div>
         </div>
         <div class="carousel-item">
-          <div class="card">
-              <div class="image-wrapper">
-                <img src="../Imagenes/CajaAradanos.png" alt="...">
+          <div class="cards-wrapper">
+            <div class="card">
+                <div class="image-wrapper">
+                  <img src="../Imagenes/ble3.png" alt="...">
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">7</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
               </div>
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+              <div class="card">
+                <div class="image-wrapper">
+                  <img src="../Imagenes/ble3.png" alt="...">
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">8</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
               </div>
-          </div>
+              <div class="card">
+                <div class="image-wrapper">
+                  <img src="../Imagenes/ble3.png" alt="...">
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">9</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>  
+          </div> 
+          <!-- acaba aqui--> 
         </div>
       </div>
-      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Previous</span>
-      </button>
-      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="visually-hidden">Next</span>
-      </button>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+    
+      
   </div>
-  </div>
-  </h1>
   </body>
 </html>
